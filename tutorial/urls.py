60edd52b2969e685c7ecc43f005a514f6765a73f
Richard Wackerbarth <richard@NFSNet.org>
2013-01-19 15:33:43 -0600

    Move endpoints to tutorial

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,6 +8,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = format_suffix_patterns(patterns('snippets.views',
     url(r'^$', 'api_root'),
+    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view(), name='snippet-detail'),
+    url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    url(r'^users/$', views.UserList.as_view(), name='user-list'),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserInstance.as_view(), name='user-detail'),
 ))
 
 urlpatterns += patterns('',
