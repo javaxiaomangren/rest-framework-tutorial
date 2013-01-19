819700a12c31e042100adf158f5e4a3bad4cb179
Richard Wackerbarth <richard@NFSNet.org>
2013-01-19 15:37:09 -0600

    URLs in Snippets app

--- snippets/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from django.conf.urls import include
-from rest_framework.urlpatterns import format_suffix_patterns
-
-# API endpoints
-urlpatterns = format_suffix_patterns(patterns('snippets.views',
-))
